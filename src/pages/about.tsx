d917ccca8e2fcb05bd67fcf4d9d72e38e4161cf6
Loloao <l1312213036@outlook.com>
2023-01-09 18:17:44 +0800

    :rotating_light: test multiple remote push

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 
 export default function about() {
-  return <Layout> </Layout>;
+  return <Layout>about page</Layout>;
 }
 
 export const Head = () => {
