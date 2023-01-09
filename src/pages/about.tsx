d60f4e52174ec0d82573621bf5e8da55015427ea
Loloao <l1312213036@outlook.com>
2023-01-09 18:10:02 +0800

    :mag: add about page seo

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Layout from "../components/Layout";
+import SEO from "../components/SEO";
 
 export default function about() {
-  return <Layout></Layout>;
+  return <Layout> </Layout>;
 }
+
+export const Head = () => {
+  return <SEO />;
+};
