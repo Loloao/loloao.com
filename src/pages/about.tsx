5aae589f789abf9f523e107e68bd02940d8e4d11
Loloao <1312213036@qq.com>
2023-01-11 17:28:20 +0800

    test pipeline

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 import SEO from "../components/SEO";
 
 export default function about() {
-  return <Layout>about page test pipeline11111</Layout>;
+  return <Layout>about page test pipeline111</Layout>;
 }
 
 export const Head = () => {
