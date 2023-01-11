7dfd9b8fd1377bef02b513e959a96827a85bea99
Loloao <1312213036@qq.com>
2023-01-11 19:22:16 +0800

    :bug: fix cant find Layout comp

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
-import Header, { MODE as MODE_VALUE } from "./Header";
-import Footer from "./Footer";
+import Header, { MODE as MODE_VALUE } from "../Header";
+import Footer from "../Footer";
 
 interface props {
   onChangeMode?: (MODE_VALUE) => void;
@@ -8,6 +8,7 @@ interface props {
 }
 
 const Content = (props: props) => {
+  // console.log(localStorage)
   return (
     <div className="dark:bg-black min-h-screen h-full dark:text-white bg-orange-50 text-slate-900 ">
       <div className="h-full min-h-screen xl:w-[1280px] sm:px-9 m-auto flex flex-col justify-between">
