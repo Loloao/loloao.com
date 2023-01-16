cad16365cf40d5d8a1ad81173990dc75dbbb8dca
Loloao <1312213036@qq.com>
2023-01-16 15:13:36 +0800

    :art: update home page profile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,12 +12,11 @@ export default function Home({ data }) {
   const latest = data.allMarkdownRemark.edges.map((v) => v.node.frontmatter);
   return (
     <Layout>
-      <h1 className="text-4xl font-bold mt-[50px]">嗨！我是 Loloao!</h1>
+      <h1 className="text-4xl font-bold mt-[50px]">嗨！我是李泱!</h1>
       <br />
       <div className="text-lg mt-6">欢迎来到我的 web 基地.</div>
       <div className="text-lg mt-6">
-        我是一名前端工程师，2017
-        年入行，目前在深圳工作。在这个博客我会分享看过的书，学习过的知识以及我的生活。我喜欢画漫画，读书以及游戏。
+        我是一名在深圳工作的前端工程师。在这个博客我会分享看过的书，学习过的知识以及我的生活。我喜欢画漫画，读书以及游戏。
       </div>
 
       <BlockWrapper title="最近更新">
